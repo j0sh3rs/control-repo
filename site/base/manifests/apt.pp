bbe9243b3e74d4dd5f91387ca4619ca6b1b030a7
j0sh3rs <github@beholdthehurricane.com>
2024-04-25 15:29:21 -0400

    Fixup

--- site/base/manifests/apt.pp
+++ site/base/manifests/apt.pp
@@ -6,11 +6,11 @@ class base::apt {
   include apt
 
   class { 'unattended_upgrades':
-    age                     => { 'max' => 5 },
-    auto                    => { 'clean' => 'always' },
-    update                  => 'always',
-    upgrade                 => 'always',
-    remove_unused_kernel    => true,
-    removed_new_unused_deps => true
+    age                    => { 'max' => 5 },
+    auto                   => { 'clean' => 'always' },
+    update                 => 'always',
+    upgrade                => 'always',
+    remove_unused_kernel   => true,
+    remove_new_unused_deps => true
   }
 }
