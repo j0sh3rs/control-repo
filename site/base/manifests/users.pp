2017aac0ebc9a651eac0f4ff084944496c11f39a
j0sh3rs <github@beholdthehurricane.com>
2024-07-14 10:59:11 -0400

    Add more managed keys

--- site/base/manifests/users.pp
+++ site/base/manifests/users.pp
@@ -9,10 +9,24 @@ class base::users {
   }
 
 
-  ssh_authorized_key { 'j0sh3rs':
+  ssh_authorized_key { 'personal-lappy':
     ensure => present,
     user   => 'j0sh3rs',
     type   => 'ecdsa-sha2-nistp521',
     key    => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBADRC5VDmRxrGEf1WplDaIRX9NlAxCSArK4UEN6B+99HtZiylpqPm9QNOUxcrActzsi84oLzVzIwkNi7BhrPSSmJogDWK4YqUqS8ZItx06g1OU1sHnByTpSJkdb2csHU0CMTMUttWaOp4Gs87EvBvcJPMeOyy1TB/ktw4K6lXtV+OJbn7g==',
   }
+
+  ssh_authorized_key { 'work-lappy':
+    ensure => present,
+    user   => 'j0sh3rs',
+    type   => 'ecdsa-sha2-nistp521',
+    key    => 'AAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBABlbFzDFFfdUPekjXtAhlASJ5ZBf6HTmKWuxR3e/w/ztinZZuTbg4CQUAjvOK2WUWKOGOS0bcyKV8eaamFz7laQhQG7uoR+Xn/4U9arfOeI42TW74929rSBTfcQMC7WNr4NnhagpLt3F8gtu6x/9+QibL58A2xen23HHjAzr4KR5SQkjw==',
+  }
+
+  ssh_authorized_key { 'bee03':
+    ensure => present,
+    user   => 'j0sh3rs',
+    type   => 'ecdsa-sha2-nistp521',
+    key    => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACX2CBi9Sth2/1lpePSF6cEk6Z0INT1rb9yNul+pTvCvV0fYfH9ncaTzY/yTgk2UxB1vnGA+fvhfHio4DYXrTyewAEPuBU6fJqLB/ScK+2I1mO0lcIdX9xUTpGpFAIUwLaQdW/ET0N9EcbRCp6X1uM/NsYKdc23m96RL04cHTu2tdErYA==',
+  }  
 }
