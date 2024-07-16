fb7f67b6370f05b2231baf47bb508cca6503e38b
j0sh3rs <github@beholdthehurricane.com>
2024-07-16 13:46:27 -0400

    Fixup work lappy ssh key

--- site/base/manifests/users.pp
+++ site/base/manifests/users.pp
@@ -20,7 +20,7 @@ class base::users {
     ensure => present,
     user   => 'j0sh3rs',
     type   => 'ecdsa-sha2-nistp521',
-    key    => 'AAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBABlbFzDFFfdUPekjXtAhlASJ5ZBf6HTmKWuxR3e/w/ztinZZuTbg4CQUAjvOK2WUWKOGOS0bcyKV8eaamFz7laQhQG7uoR+Xn/4U9arfOeI42TW74929rSBTfcQMC7WNr4NnhagpLt3F8gtu6x/9+QibL58A2xen23HHjAzr4KR5SQkjw==',
+    key    => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBABlbFzDFFfdUPekjXtAhlASJ5ZBf6HTmKWuxR3e/w/ztinZZuTbg4CQUAjvOK2WUWKOGOS0bcyKV8eaamFz7laQhQG7uoR+Xn/4U9arfOeI42TW74929rSBTfcQMC7WNr4NnhagpLt3F8gtu6x/9+QibL58A2xen23HHjAzr4KR5SQkjw==',
   }
 
   ssh_authorized_key { 'bee03':
@@ -28,5 +28,5 @@ class base::users {
     user   => 'j0sh3rs',
     type   => 'ecdsa-sha2-nistp521',
     key    => 'AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACX2CBi9Sth2/1lpePSF6cEk6Z0INT1rb9yNul+pTvCvV0fYfH9ncaTzY/yTgk2UxB1vnGA+fvhfHio4DYXrTyewAEPuBU6fJqLB/ScK+2I1mO0lcIdX9xUTpGpFAIUwLaQdW/ET0N9EcbRCp6X1uM/NsYKdc23m96RL04cHTu2tdErYA==',
-  }  
+  }
 }
