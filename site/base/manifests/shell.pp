4ee2ccb73a342654662f854da406ed46d4c98033
j0sh3rs <github@beholdthehurricane.com>
2024-09-21 19:46:58 -0400

    Fix more

--- site/base/manifests/shell.pp
+++ site/base/manifests/shell.pp
@@ -5,7 +5,7 @@ class base::shell {
   file { '/home/j0sh3rs/.bashrc':
     ensure => 'present',
     source => 'puppet:///modules/base/.bashrc',
-    mode   => 0644,
+    mode   => '0644',
     owner  => 'j0sh3rs',
     group  => 'j0sh3rs'
   }
