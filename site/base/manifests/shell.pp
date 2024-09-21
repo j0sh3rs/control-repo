8fbe6444b2c010073df058d65c7a8ec3aedd48ec
j0sh3rs <github@beholdthehurricane.com>
2024-09-21 19:46:19 -0400

    Fix linting

--- site/base/manifests/shell.pp
+++ site/base/manifests/shell.pp
@@ -3,8 +3,10 @@
 #
 class base::shell {
   file { '/home/j0sh3rs/.bashrc':
-    ensure => 'present'
-    source => 'puppet:///modules/base/.bashrc'
-    mode   => 0644
+    ensure => 'present',
+    source => 'puppet:///modules/base/.bashrc',
+    mode   => 0644,
+    owner  => 'j0sh3rs',
+    group  => 'j0sh3rs'
   }
 }
